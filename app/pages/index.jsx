5f74d6b03ab9df487e9969238c4227db8f506796
iamcco <ooiss@qq.com>
2018-10-28 19:20:27 +0800

    add title

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -145,19 +145,40 @@ export default class PreviewPage extends React.Component {
     return (
       <React.Fragment>
         <Head>
-          <title>{`「${name || 'Preview Page'}」`}</title>
+          <title>{`「${name}」`}</title>
           <link rel="shortcut icon" type="image/ico" href="/_static/favicon.ico" />
+          <link rel="stylesheet" href="/_static/page.css" />
           <link rel="stylesheet" href="/_static/markdown.css" />
           <link rel="stylesheet" href="/_static/highlight.css" />
           <link rel="stylesheet" href="/_static/katex@0.5.1.css" />
           <script type="text/javascript" src="/_static/tweenlite.min.js"></script>
         </Head>
-        <section
-          className="markdown-body"
-          dangerouslySetInnerHTML={{
-            __html: content
-          }}
-        />
+        <div id="page-ctn">
+          <header id="page-header">
+            <h3>
+              <svg
+                viewBox="0 0 16 16"
+                version="1.1"
+                width="16"
+                height="16"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5h4v1H3V5zm0 3h4V7H3v1zm0 2h4V9H3v1zm11-5h-4v1h4V5zm0 2h-4v1h4V7zm0 2h-4v1h4V9zm2-6v9c0 .55-.45 1-1 1H9.5l-1 1-1-1H2c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h5.5l1 1 1-1H15c.55 0 1 .45 1 1zm-8 .5L7.5 3H2v9h6V3.5zm7-.5H9.5l-.5.5V12h6V3z"
+                >
+                </path>
+              </svg>
+              { name }
+            </h3>
+          </header>
+          <section
+            className="markdown-body"
+            dangerouslySetInnerHTML={{
+              __html: content
+            }}
+          />
+        </div>
       </React.Fragment>
     )
   }
