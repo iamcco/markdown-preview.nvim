e493f505ec839dd44eb0d9db52bf2a8f9eabe052
iamcco <ooiss@qq.com>
2019-03-29 15:06:45 +0800

    fix local image display in html element

--- app/pages/image.js
+++ app/pages/image.js
@@ -1,3 +1,16 @@
+function resolveHtmlImage (tokens, idx) {
+  let content = tokens[idx].content || ''
+
+  content = content.replace(/<img\s+([^>]*?)src\s*=\s*(["'])([^\2>]+?)\2([^>]*)>/gm, (m, g1, g2, g3, g4) => {
+    if (/^(http|\/\/|data:)/.test(g3)) {
+      return m
+    }
+    return `<img ${g1}src="/_local_image_${encodeURIComponent(g3)}"${g4}>`
+  })
+
+  return content
+}
+
 function resolveImage (tokens, idx) {
   const src = tokens[idx].attrs[0][1]
   const alt = tokens[idx].content
@@ -9,4 +22,6 @@ function resolveImage (tokens, idx) {
 
 export default function localImage (md) {
   md.renderer.rules.image = resolveImage
+  md.renderer.rules.html_block = resolveHtmlImage
+  md.renderer.rules.html_inline = resolveHtmlImage
 }
