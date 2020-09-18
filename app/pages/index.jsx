80676c4d31cf5e41710f72717612fbf2b6280577
Evan Ott <evan1533@vt.edu>
2020-09-18 15:53:47 -0400

    Conditionally render header based on 'disable_filename' field in 'mkdp_preview_options'

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -189,7 +189,8 @@ export default class PreviewPage extends React.Component {
       })(name),
       content: this.md.render(content.join('\n')),
       pageTitle,
-      contentEditable: options.content_editable
+      contentEditable: options.content_editable,
+      disableFilename: options.disable_filename
     }, () => {
       try {
         // eslint-disable-next-line
@@ -215,7 +216,7 @@ export default class PreviewPage extends React.Component {
   }
 
   render() {
-    const { content, name, pageTitle, contentEditable } = this.state
+    const { content, name, pageTitle, contentEditable, disableFilename } = this.state
     return (
       <React.Fragment>
         <Head>
@@ -240,24 +241,26 @@ export default class PreviewPage extends React.Component {
           <script type="text/javascript" src="/_static/full.render.js"></script>
         </Head>
         <div id="page-ctn" contentEditable={contentEditable ? 'true' : 'false'}>
-          <header id="page-header">
-            <h3>
-              <svg
-                viewBox="0 0 16 16"
-                version="1.1"
-                width="16"
-                height="16"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5h4v1H3V5zm0 3h4V7H3v1zm0 2h4V9H3v1zm11-5h-4v1h4V5zm0 2h-4v1h4V7zm0 2h-4v1h4V9zm2-6v9c0 .55-.45 1-1 1H9.5l-1 1-1-1H2c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h5.5l1 1 1-1H15c.55 0 1 .45 1 1zm-8 .5L7.5 3H2v9h6V3.5zm7-.5H9.5l-.5.5V12h6V3z"
+          { !disableFilename &&
+            <header id="page-header">
+              <h3>
+                <svg
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  width="16"
+                  height="16"
+                  aria-hidden="true"
                 >
-                </path>
-              </svg>
-              {name}
-            </h3>
-          </header>
+                  <path
+                    fill-rule="evenodd"
+                    d="M3 5h4v1H3V5zm0 3h4V7H3v1zm0 2h4V9H3v1zm11-5h-4v1h4V5zm0 2h-4v1h4V7zm0 2h-4v1h4V9zm2-6v9c0 .55-.45 1-1 1H9.5l-1 1-1-1H2c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h5.5l1 1 1-1H15c.55 0 1 .45 1 1zm-8 .5L7.5 3H2v9h6V3.5zm7-.5H9.5l-.5.5V12h6V3z"
+                  >
+                  </path>
+                </svg>
+                {name}
+              </h3>
+            </header>
+          }
           <section
             className="markdown-body"
             dangerouslySetInnerHTML={{
