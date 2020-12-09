96c0bc72252f87c4fcafaa672352a91730dee61d
JF Lavoie <1970141+jf-lavoie@users.noreply.github.com>
2020-12-08 21:47:59 -0500

    fix wsl2 detection (#270)
    * fix wsl2 detection
    
    * fix wsl2 detection in src/util/opener.ts
    
    Co-authored-by: Jean-Francois Lavoie <jflavoie@genvidtech.com>

--- src/util/opener.ts
+++ src/util/opener.ts
@@ -16,7 +16,7 @@ module.exports = function opener(
   // this specific case we need to treat it as actually being Windows.
   // The "Windows-way" of opening things through cmd.exe works just fine here,
   // whereas using xdg-open does not, since there is no X Windows in WSL.
-  if (platform === 'linux' && os.release().indexOf('Microsoft') !== -1) {
+  if (platform === 'linux' && os.release().toLowerCase().indexOf('microsoft') !== -1) {
     platform = 'win32'
   }
 
